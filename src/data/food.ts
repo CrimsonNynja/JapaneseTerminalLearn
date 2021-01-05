9c61862660a941091b8293a7bb11930c6bd1c330
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-06 01:57:53 +1100

    added vegetable and water to food translation

--- src/data/food.ts
+++ src/data/food.ts
@@ -1,5 +1,7 @@
 const Food = {
   'meat': { 'kanji': '肉', 'hiragana': 'にく' },
+  'vegetable': { 'kanji': '野菜', 'hiragana': 'やさい' },
+  'water': { 'kanji': '水', 'hiragana': 'みず' },
 };
 
 export default Food;
