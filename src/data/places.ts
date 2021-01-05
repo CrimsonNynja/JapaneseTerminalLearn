f6fabc239a30c66fbc3b1928bd4c6e7e674924fd
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-06 01:46:48 +1100

    adde places translation file

---
+++ src/data/places.ts
@@ -0,0 +1,9 @@
+const Places = {
+  'tokyo': { 'kanji': '東京', 'hiragana': 'とうきょう' },
+  'kyoto': { 'kanji': '京都', 'hiragana': 'きょうと' },
+  'osaka': { 'kanji': '大阪', 'hiragana': 'おおさか' },
+  'japan': { 'kanji': '日本', 'hiragana': 'にほん' },
+  'china': { 'kanji': '中国', 'hiragana': 'ちゅうごく' },
+};
+
+export default Places;
