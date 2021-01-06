19c4027ab68de2b54e09bcd05d820f1a82a935a7
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-07 01:16:47 +1100

    aded 2-10 in numbers

--- src/data/numbers.ts
+++ src/data/numbers.ts
@@ -4,6 +4,51 @@ const Numbers = [
     'kanji': '一',
     'kanamoji': 'いち',
   },
+  {
+    'english': ['two', '2'],
+    'kanji': '二',
+    'kanamoji': 'に',
+  },
+  {
+    'english': ['three', '3'],
+    'kanji': '三',
+    'kanamoji': 'さん',
+  },
+  {
+    'english': ['four', '4'],
+    'kanji': '四',
+    'kanamoji': ['し', 'よん'],
+  },
+  {
+    'english': ['five', '5'],
+    'kanji': '五',
+    'kanamoji': 'ご',
+  },
+  {
+    'english': ['six', '6'],
+    'kanji': '六',
+    'kanamoji': 'ろく',
+  },
+  {
+    'english': ['seven', '7'],
+    'kanji': '七',
+    'kanamoji': ['しち', 'なな'],
+  },
+  {
+    'english': ['eight', '8'],
+    'kanji': '八',
+    'kanamoji': 'はち',
+  },
+  {
+    'english': ['nine', '9'],
+    'kanji': '九',
+    'kanamoji': 'きゅう',
+  },
+  {
+    'english': ['ten', '10'],
+    'kanji': '十',
+    'kanamoji': 'じゅう',
+  },
 ];
 
 export default Numbers;
