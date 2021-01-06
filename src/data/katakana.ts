f5ddb2935a5b909e9a958457096b25bc9106db0f
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-07 00:49:03 +1100

    added katakana k and g sounds

--- src/data/katakana.ts
+++ src/data/katakana.ts
@@ -4,6 +4,16 @@ const Katakana = [
   { 'english': 'i', 'kanamoji': 'イ' },
   { 'english': 'o', 'kanamoji': 'オ' },
   { 'english': 'u', 'kanamoji': 'ウ' },
+  { 'english': 'ka', 'kanamoji': 'カ' },
+  { 'english': 'ki', 'kanamoji': 'キ' },
+  { 'english': 'ku', 'kanamoji': 'ク' },
+  { 'english': 'ke', 'kanamoji': 'ケ' },
+  { 'english': 'ko', 'kanamoji': 'コ' },
+  { 'english': 'ga', 'kanamoji': 'ガ' },
+  { 'english': 'gi', 'kanamoji': 'ギ' },
+  { 'english': 'gu', 'kanamoji': 'グ' },
+  { 'english': 'ge', 'kanamoji': 'ゲ' },
+  { 'english': 'go', 'kanamoji': 'ゴ' },
 ];
 
 export default Katakana;
