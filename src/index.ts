3ab527516a4402d2396306b5f74cf0a3d2da6fc8
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-07 01:28:05 +1100

    improved questions logic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 import { getLine, write } from './utils/inputOutput.ts';
 import Hiragana from './data/hiragana.ts';
+import Katakana from './data/katakana.ts';
+import Places from './data/places.ts';
+import Numbers from './data/numbers.ts';
+import Food from './data/food.ts';
+import Family from './data/family.ts';
+import Animals from './data/animals.ts';
 
 const getRandomItem = (object: any) => {
   const randomIndex = Math.floor(Math.random() * object.length);
+  let question = object[randomIndex].kanji
+  if (!question) {
+    question = object[randomIndex].kanamoji;
+  }
   return {
-    question: object[randomIndex].kanamoji,
+    question,
     answer: object[randomIndex].english,
   };
+};
+
+const isAnswerCorrect = (answer: string | string[], given: string) => {
+  if(Array.isArray(answer)) {
+    if (answer.includes(given)) {
+      return true;
+    }
+  } else {
+    if (answer === given) {
+      return true;
+    }
+  }
+  return false;
 };
 
 write('enter question count: ');
@@ -14,10 +37,18 @@ const questionCount = await getLine();
 
 let correctCount = 0;
 for (let i = 0; i < Number(questionCount); i++) {
-  const { question, answer } = getRandomItem(Hiragana);
+  const { question, answer } = getRandomItem([
+    ...Hiragana,
+    ...Katakana,
+    ...Places,
+    ...Numbers,
+    ...Food,
+    ...Family,
+    ...Animals,
+  ]);
   write('enter translation ' + question + ': ');
   const line = await getLine();
-  if (line === answer) {
+  if (isAnswerCorrect(answer, line)) {
     console.log('✅');
     correctCount += 1;
   } else {
