bed6e4290819f64db1fedf5072c5c2b513fa9113
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-08 23:48:55 +1100

    added mum, dad, children, family

--- src/data/family.ts
+++ src/data/family.ts
@@ -3,7 +3,27 @@ const Family = [
     'english': ['younger sister', 'little sister'],
     'kanji': '妹',
     'kanamoji': 'いもうと',
-  }
+  },
+  {
+    'english': ['mum', 'mother'],
+    'kanji': '母',
+    'kanamoji': 'はは',
+  },
+  {
+    'english': ['dad', 'father'],
+    'kanji': '父',
+    'kanamoji': 'ちち',
+  },
+  {
+    'english': 'children',
+    'kanji': '子供',
+    'kanamoji': 'こども',
+  },
+  {
+    'english': 'family',
+    'kanji': '家族',
+    'kanamoji': 'かぞく',
+  },
 ];
 
 export default Family;
