ea86d66fa6b61c0c270a671a6b64eea1dcc6cec3
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-17 19:55:36 +1100

    added child and missing jlp levels

--- src/data/family.ts
+++ src/data/family.ts
@@ -3,6 +3,7 @@ const Family = [
     english: ['younger sister', 'little sister'],
     kanji: '妹',
     kanamoji: 'いもうと',
+    jlp: 5,
   },
   {
     english: ['mum', 'mother'],
@@ -15,29 +16,40 @@ const Family = [
     kanamoji: 'ちち',
   },
   {
-    english: 'children',
+    english: ['children', 'child'],
     kanji: '子供',
     kanamoji: 'こども',
+    jlp: 5,
   },
   {
     english: 'family',
     kanji: '家族',
     kanamoji: 'かぞく',
+    jlp: 5,
   },
   {
     english: 'adult',
     kanji: '大人',
     kanamoji: 'おとな',
+    jlp: 5,
   },
   {
     english: ['I', 'i', 'me'],
     kanji: '私',
     kanamoji: 'わたし',
+    jlp: 5,
   },
   {
     english: ['younger brother', 'little brother'],
     kanji: '弟',
     kanamoji: 'おとうと',
+    jlp: 5,
+  },
+  {
+    english: ['child', 'kid'],
+    kanji: '子',
+    kanamoji: 'こ',
+    jlp: 5,
   },
 ];
 
