cbb3bb51a5f71ea9ec96d9129ab1f6d3ac9d88a6
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-17 19:56:00 +1100

    added chair, table and missing jlp levels

--- src/data/house.ts
+++ src/data/house.ts
@@ -3,11 +3,13 @@ const House = [
     english: 'window',
     kanji: '窓',
     kanamoji: 'まど',
+    jlp: 5,
   },
   {
     english: 'room',
     kanji: '部屋',
     kanamoji: 'へや',
+    jlp: 5,
   },
   {
     english: 'desk',
@@ -18,21 +20,36 @@ const House = [
     english: 'kitchen',
     kanji: '台所',
     kanamoji: 'だいどころ',
+    jlp: 5,
   },
   {
     english: ['bath', 'bathroom'],
     kanji: '風呂',
     kanamoji: 'ふろ',
+    jlp: 5,
   },
   {
     english: 'garden',
     kanji: '庭',
     kanamoji: 'にわ',
+    jlp: 5,
   },
   {
     english: 'house',
     kanji: '家',
     kanamoji: 'いえ',
+    jlp: 5,
+  },
+  {
+    english: 'chair',
+    kanji: '椅子',
+    kanamoji: 'いす',
+    jlp: 5,
+  },
+  {
+    english: 'table',
+    kanamoji: 'テーブル',
+    jlp: 5,
   },
 ];
 
