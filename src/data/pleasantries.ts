cd9809f91fa1210e7d03919dd9a20c78dd0afe3b
Hudson Cassidy <hudoc96@hotmail.com>
2021-01-19 13:57:19 +1100

    added sorry, bye, thanks to pleasantries

--- src/data/pleasantries.ts
+++ src/data/pleasantries.ts
@@ -7,6 +7,20 @@ const Pleasantries = [
     english: 'welcome',
     kanamoji: 'ようこそ',
   },
+  {
+    english: ['sorry', 'excuse me', 'pardon me'],
+    kanamoji: ['すいません', 'すみません'],
+    jlp: 3,
+  },
+  {
+    english: ['see you', 'bye'],
+    kanamoji: 'じゃあね',
+  },
+  {
+    english: ['thank you', 'thanks'],
+    kanamoji: 'どうも',
+    jlp: 5,
+  }
 ];
 
 export default Pleasantries;
