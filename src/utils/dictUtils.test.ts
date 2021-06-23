6bb4dffbb8b49f77a93dbfee396f1dd924417c4b
Hudson Cassidy <hudoc96@hotmail.com>
2021-06-24 01:49:04 +1000

    updated std test library. rewrote 1 test, remove the others, as a refactor will be happening here soon

---
+++ src/utils/dictUtils.test.ts
@@ -0,0 +1,13 @@
+import { assertArrayIncludes, assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
+import { combineDicts } from './dictUtils.ts';
+import { Dict, DictWord } from '../types/dict.ts';
+
+Deno.test("combineDicts test", () => {
+  Deno.test("run on 1 data point", () => {
+    const word: DictWord = { english: "a", kanamoji: "ア" };
+    const chapter: Dict = [word];
+    const dict = combineDicts(chapter, chapter);
+    assertEquals(dict.length, 1);
+    assertArrayIncludes(dict, [word]);
+  });
+});
