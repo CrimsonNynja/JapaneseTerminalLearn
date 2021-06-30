4a56d359ab967d58b0d45e5a8776d93de38cb391
Hudson Cassidy <hudoc96@hotmail.com>
2021-06-30 22:56:48 +1000

    fixed deprecation notice and potential nullable in InputOutput

--- src/utils/inputOutput.ts
+++ src/utils/inputOutput.ts
@@ -1,13 +1,17 @@
+import { writeAll } from "https://deno.land/std@0.99.0/io/util.ts";
+
 export const getLine = async () => {
   const buffer: Uint8Array = new Uint8Array(1024);
-  const bytes: number = <number> await Deno.stdin.read(buffer);
-  const line: string = new TextDecoder().decode(buffer.subarray(0, bytes))
-    .trim();
+  const bytes: number|null = await Deno.stdin.read(buffer);
+  if (bytes) {
+    const line: string = new TextDecoder().decode(buffer.subarray(0, bytes)).trim();
+    return line;
+  }
 
-  return line;
+  return '';
 };
 
 export const write = async (message: string) => {
   const text: Uint8Array = new TextEncoder().encode(message);
-  await Deno.writeAll(Deno.stdout, text);
+  await writeAll(Deno.stdout, text);
 };
